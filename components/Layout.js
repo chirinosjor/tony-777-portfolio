ea1a2d51470c7169dd7871b8f27afbd10701cb18
Jorge Chirinos <chirinosjor@gmail.com>
2022-11-14 17:14:13 -0300

    chore: fix alignment

--- components/Layout.js
+++ components/Layout.js
@@ -5,12 +5,14 @@ import ArtistInfo from './ArtistInfo';
 
 function Layout ({ children }) {
   return (
-    <div className='bg-scroll bg-[url("/assets/header.jpg")] h-[60vh] w-full bg-cover'>
-      <Header />
-      <ArtistInfo />
-      <Navbar />
-      <main>{children}</main>
-    </div>
+    <>
+      <div className='bg-scroll bg-[url("/assets/header.jpg")] h-[60vh] w-full bg-cover'>
+        <Header />
+        <ArtistInfo />
+        <Navbar />
+        <main className='ml-[10vh]'>{children}</main>
+      </div>
+    </>
   );
 }
 
