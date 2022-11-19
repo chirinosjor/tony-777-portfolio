eb00444bdee88254c591890137e1cb37c0e14ce4
Jorge Chirinos <chirinosjor@gmail.com>
2022-11-19 13:52:12 -0300

    Add title styles

--- components/ArtistInfo.js
+++ components/ArtistInfo.js
@@ -1,13 +1,22 @@
 import React from 'react';
+import Image from 'next/image';
 
 function ArtistInfo () {
   return (
     <div className='flex flex-col ml-[80vh]'>
-      <h1 className='text-6xl text-white font-bold mt-[30vh]'>Tony 777</h1>
-      <div className='flex gap-4'>
-        <p className='text-2xl text-white'>6000 followers</p>
-        <p className='text-2xl text-white'>21000 likes</p>
-        <p className='text-2xl text-white'>600000 plays</p>
+      <h1 className='text-6xl text-white font-bold mt-[25vh] mb-6'>Tony 777</h1>
+      <div className='flex gap-10 ml-4 mb-2'>
+        <p className='text-base text-white'>6000 FOLLOWERS</p>
+        <p className='text-base text-white'>21000 LIKES</p>
+        <p className='text-base text-white'>600000 PLAYS</p>
+      </div>
+      <div className='flex gap-8 ml-4 mb-14'>
+        <button
+          className='
+          w-40 bg-black text-white font-bold py-2 px-4 rounded-full mt-6 flex gap-4'>
+          <Image src='/assets/icons/play.png' width={25} height={25} />
+          PLAY ALL
+        </button>
       </div>
     </div>
   );
--- components/Navbar.js
+++ components/Navbar.js
@@ -5,7 +5,7 @@ function Navbar () {
   return (
     <>
       <nav>
-        <ul className='flex gap-[150px] ml-[80vh] mt-[10vh]'>
+        <ul className='flex gap-[150px] ml-[80vh]'>
           <li className='text-2xl text-white border-b-8'><Link href="/overview" ><p className='p-6'>Overview</p></Link></li>
           <li className='text-2xl text-white border-b-8'><Link href="/music" ><p className='p-6'>Music</p></Link></li>
           <li className='text-2xl text-white border-b-8'><Link href="/videos" ><p className='p-6'>Videos</p></Link></li>
