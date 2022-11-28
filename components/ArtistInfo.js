5e19d1d0904cf76afe49ab111cf14b4dbe5c5401
Jorge Chirinos <chirinosjor@gmail.com>
2022-11-27 23:45:42 -0300

    feat: add sample audio

--- components/ArtistInfo.js
+++ components/ArtistInfo.js
@@ -1,22 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Image from 'next/image';
+import useSound from 'use-sound';
 
 function ArtistInfo () {
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [play, { pause }] = useSound('/sound.mp3');
+
+  const handlePlay = () => {
+    setIsPlaying(true);
+    play();
+  };
+
+  const handleStop = () => {
+    setIsPlaying(false);
+    pause();
+  };
+
+  const statsStyles = 'text-sm text-white';
+  const socialsStyles = 'bg-black text-white font-bold py-2 px-4 rounded-full mt-6 flex gap-4 border-white border-2 hover:border-darkRebelRed';
+
   return (
     <div className='flex flex-col ml-[80vh]'>
       <h1 className='text-6xl text-lightRebelRed font-bold mt-[25vh] mb-6'>TONY 777</h1>
       <div className='flex gap-10 ml-4 mb-2'>
-        <p className='text-sm text-white'>6000 FOLLOWERS</p>
-        <p className='text-sm text-white'>21000 LIKES</p>
-        <p className='text-sm text-white'>600000 PLAYS</p>
+        <p className={statsStyles}>6000 FOLLOWERS</p>
+        <p className={statsStyles}>21000 LIKES</p>
+        <p className={statsStyles}>600000 PLAYS</p>
       </div>
       <div className='flex gap-8 ml-4 mb-10'>
         <button
+          onClick={() => (isPlaying ? handleStop() : handlePlay())}
           className='
-          w-40 bg-black text-white font-bold py-2 px-4 rounded-full mt-6 flex gap-4 border-white border-2'>
-          <Image src='/assets/icons/play.png' width={25} height={25} />
+          hover:border-darkRebelRed w-40 bg-black text-white font-bold py-2 px-4 rounded-full mt-6 flex gap-4 border-white border-2'>
+          {!isPlaying ? <Image src='/assets/icons/play.png' width={25} height={25} /> : <Image src='/assets/icons/pause.png' width={25} height={25} />}
           PLAY ALL
         </button>
+        <div className="wrapper flex gap-4">
+          <a href="#" className={socialsStyles}>
+            <span><i className="fab fa-facebook-f"></i></span>
+          </a>
+          <a href="#" className={socialsStyles}>
+            <span><i className="fab fa-twitter"></i></span>
+          </a>
+          <a href="#" className={socialsStyles}>
+            <span><i className="fab fa-instagram"></i></span>
+          </a>
+          <a href="#" className={socialsStyles}>
+            <span><i className="fab fa-youtube"></i></span>
+          </a>
+        </div>
       </div>
     </div>
   );
--- components/Navbar.js
+++ components/Navbar.js
@@ -2,18 +2,19 @@ import Link from 'next/link';
 import React from 'react';
 
 function Navbar () {
-  const linkStyles = 'px-6 text-xl text-lightRebelRed border-b-4 border-lightRebelRed';
+  const linkStyles = 'px-6 text-xl text-white hover:text-lightRebelRed border-b-4 border-lightRebelRed';
+  const navLink = (label, route) => (
+    <li className={linkStyles}><Link href={route} ><p className='p-6'>{label}</p></Link></li>
+  );
   return (
-    <>
-      <nav>
-        <ul className='flex gap-[150px] ml-[80vh]'>
-          <li className={linkStyles}><Link href="/" ><p className='p-6'>Overview</p></Link></li>
-          <li className={linkStyles}><Link href="/music" ><p className='p-6'>Music</p></Link></li>
-          <li className={linkStyles} ><Link href="/videos" ><p className='p-6'>Videos</p></Link></li>
-          <li className={linkStyles}><Link href="/shows" ><p className='p-6'>Shows</p></Link></li>
-        </ul>
-      </nav>
-    </>
+    <nav>
+      <ul className='flex gap-[150px] ml-[80vh]'>
+        {navLink('Overview', '/')}
+        {navLink('Music', '/music')}
+        {navLink('Videos', '/videos')}
+        {navLink('Shows', '/shows')}
+      </ul>
+    </nav>
   );
 }
 
